4abfbf3425b126ad65d5312c883d47841bd9643b
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-20 16:45:44 -0500

    adelanto

---
+++ twitter/js/main.js
@@ -0,0 +1,14 @@
+function todoList(){
+
+	var item = document.getElementById("todoInput").value;
+	var text = document.createTextNode(item);
+	var newItem = document.createElement("li");
+	newItem.setAttribute("class", "list-group-item");
+	var checkbox = document.createElement("input");
+ 	checkbox.type ="checkbox";
+ 	checkbox.value = 1;
+ 	newItem.appendChild(checkbox);
+ 	newItem.appendChild(text);
+	document.getElementById("todoList").appendChild(newItem);
+
+}
