2d8bddc08a025cd840cf8c41b2ecf6d86bccd20e
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-21 12:58:46 -0500

    sad face

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -1,14 +1,14 @@
 function todoList(){
-
-	var item = document.getElementById("todoInput").value;
+	// crear la lista con checkbox
+	var item = document.getElementById("areaTexto").value;
 	var text = document.createTextNode(item);
 	var newItem = document.createElement("li");
 	var checkbox = document.createElement("input");
  	checkbox.type ="checkbox";
  	checkbox.value = 1;
  	newItem.appendChild(checkbox);
-
  	newItem.appendChild(text);
 	document.getElementById("todoList").appendChild(newItem);
-
+	// button
+	
 }
