dcfcd37f3087910b47dd008ec5cf7ed9e0a2e47f
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-21 16:53:59 -0500

    :)

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -6,12 +6,21 @@ function todoList(){
 	var checkbox = document.createElement("input");
  	checkbox.type ="checkbox";
  	checkbox.value = 1;
- 	
 	document.getElementById("todoList").appendChild(newItem);
-	// button
+	// AGREGAR EL ICON
 	var icono = document.createElement("i");
 		icono.setAttribute("class","fa fa-trash");
 	newItem.appendChild(checkbox);
  	newItem.appendChild(text);
  	newItem.appendChild(icono);
+ 	icono.onclick = function(){
+ 		item.parentElement.removeChild(item);
+ 	}
+ 	checkbox.onclick = function(){
+ 		if(checkbox.checked == true){
+ 			text.parentElement.classList.add("tachado");
+ 		}else{
+ 			text.parentElement.classList.remove("tachado");
+ 		}
+ 	}
 }
