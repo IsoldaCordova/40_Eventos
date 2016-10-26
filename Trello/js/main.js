5381b9c556dbdf382434aef8707550b260881a6d
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-26 12:58:33 -0500

    ya le entendi mas (cara de felicidad), aun no terminado

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -2,22 +2,41 @@ var listaUno = document.getElementById("boton");
 listaUno.addEventListener("click", añadirLista);
 var contenedor = document.getElementById("lista");
 
-function añadirLista(){
-	// primerGuardar
-	var listaName = document.createElement("input");
-		listaName.setAttribute("placeholder","añadir lista");
 
+
+function añadirLista(){
+	// crear listas
+	var listaName = document.createElement("input");
+		listaName.setAttribute("placeholder","añadir titulo");
 	var boton = document.createElement("button");
 		boton.setAttribute("type","button");
 		boton.textContent = ("Guardar");
 	contenedor.appendChild(listaName);
 	contenedor.appendChild(boton);
+ 	// Dar click en boton guardar y se borra el boton
+ 	boton.addEventListener("click",crearTitulo);
  	
- 	boton.onclick = function(){
- 		var item = document.createElement("textarea").value;
- 		boton.parentElement.removeChild(boton);
- 		var text = document.createTextNode(item);
- 		var newItem = document.createElement("ul");
- 	}
+ 	function crearTitulo(){
+ 		var tituloName = document.getElementById("tema");
+ 		var titulo =  document.createElement("h2");
+ 			titulo.innerHTML = listaName.value;
+ 		var texto = document.createElement("textarea");
+			texto.innerHTML = listaName.value;
+ 		var botonDos = document.createElement("button");
+ 			botonDos.setAttribute("type","button");
+ 			botonDos.style.display="block";
+			botonDos.textContent = ("Guardar");
+		tituloName.appendChild(titulo);
+		tituloName.appendChild(texto);
+		tituloName.appendChild(botonDos);
 
+		botonDos.EventListener("click",crearListas);
+
+		function crearListas(){
+			var tema = document.createElement("h3");
+				tema.innerHTML = listaName.value;
+
+			texto.appendChild(tema);
+		}
+ 	}
  }
